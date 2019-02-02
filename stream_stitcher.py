7952819f65dbd95ab5fe4bc1844ead557483c152
GrantPerkins <gcperk20@gmail.com>
2019-02-02 15:36:18 -0500

    Fix? stream stitching, bad hardware atm

--- stream_stitcher.py
+++ stream_stitcher.py
@@ -4,59 +4,74 @@ import threading
 from queue import Queue
 import math
 
-frame_left, frame_right = None, None
 
-def frame_putter(source, queue):
-    side = source
-    source = cv2.VideoCapture(source)
-    source.set(3, 320)
-    source.set(4, 240)
-    # source.set(12, 16)
-    source.set(cv2.CAP_PROP_AUTOFOCUS, 0)
-    ret, frame = source.read()
-    while True:
-        if ret:
+class FrameGetter:
+    def __init__(self):
+        self._frame_left, self._frame_right = None, None
+        t0 = threading.Thread(target=self.frame_putter, args=(1,))
+        t1 = threading.Thread(target=self.frame_putter, args=(2,))
+        t0.start()
+        t1.start()
+
+    def frame_putter(self, source):
+        side = source
+        source = cv2.VideoCapture(side, cv2.CAP_DSHOW)
+        source.set(3, 320)
+        source.set(4, 240)
+        source.set(10, .01)
+        source.set(15, 16)
+        source.set(cv2.CAP_PROP_AUTOFOCUS, 0)
+        ret, frame = source.read()
+        while True:
+            if ret:
+                if side == 2:
+                    self._frame_left = frame
+                    print("left", self._frame_left.shape)
+                else:
+                    self._frame_right = frame
+                    print("right", self._frame_right.shape)
             ret, frame = source.read()
-            if side == 0:
-                pass
-        else:
-            pass
-        time.sleep(.1)
+            time.sleep(.1)
 
-# TODO: write to left and write frame objects with each frame, merge them
+    def get_left(self):
+        return self._frame_left
 
-t1 = threading.Thread(target=frame_putter, args=(1, queue))
-t2 = threading.Thread(target=frame_putter, args=(2, queue))
-t1.start()
-t2.start()
+    def get_right(self):
+        return self._frame_right
 
-stitcher = cv2.createStitcher()
-frames = dict()
-a, img = None, None
-while True:
-    while queue.empty():
-        time.sleep(0.01)
-    index, frame = queue.get()
-    if frames.get(math.floor(index)) is not None:  # mate present
-        mate = frames.get(math.floor(index))
-        # cv2.imshow("img", frame)
 
-        if int(index) == index:  # left
-            a, img = stitcher.stitch((frame, mate))
-        else:  # right
-            a, img = stitcher.stitch((mate, frame))
-        #
-        # cv2.imshow("mate", mate)
-        # key = cv2.waitKey(5)
-        # if key == 27:
-        #     break
-        frames.pop(math.floor(index))
 
-        if a == 0:
+def main():
+    picture_nb = 0
+    start = time.time()
+    getter = FrameGetter()
+    stitcher = cv2.createStitcher()
+    time.sleep(8)
+    """
+    for i in range(5):
+        one = cv2.imread("pairs/left_{}.png".format(i))
+        two = cv2.imread("pairs/right_{}.png".format(i))
+        status, img = stitcher.stitch((one, two))
+        cv2.imwrite("pairs/stitched_{}.png".format(i), img)
+    # """
+    while True:
+        
+        status, img = stitcher.stitch((getter.get_right(), getter.get_left()))
+        if status == 0:
             cv2.imshow("frame", img)
-            key = cv2.waitKey(10)
+            key = cv2.waitKey(1)
             if key == 27:
                 break
-            print(img.shape)
-    else:  # new mate
-        frames.update({math.floor(index): frame})
+        else:
+            print("failing")
+            
+        if start + 5 < time.time() and picture_nb < 5:
+
+            cv2.imwrite("pairs/left_{}.png".format(picture_nb), getter.get_left())
+            cv2.imwrite("pairs/right_{}.png".format(picture_nb), getter.get_right())
+            picture_nb += 1
+        time.sleep(.1)
+        # """
+
+if __name__ == "__main__":
+    main()
